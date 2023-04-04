25d19d747b9be2ae1c557e280d2762c628383f33
sumit6675 <sumitchimkar0@gmail.com>
2023-04-04 13:25:02 +0530

    update routes folder

--- Routes/users.routes.js
+++ Routes/users.routes.js
@@ -11,12 +11,12 @@ userRoute.post("/register", async (req, res) => {
     if (name && email && password) {
       const CheckEmail = await RegisterModel.find({ email: email });
       if (CheckEmail.length > 0) {
-        res.send("email alredy exist");
+        res.status(401).json({msg:"email alredy exist"});
       } else {
         bcrypt.hash(password, 8, async (err, hash) => {
           if (err) {
             console.log("err", err);
-            res.send(err);
+            res.status(401).json({msg:err});
           }
           const user = new RegisterModel({
             name,
@@ -24,15 +24,15 @@ userRoute.post("/register", async (req, res) => {
             password: hash,
           });
           await user.save();
-          res.send("Registerd");
+          res.status(201).json({msg:"Registerd"});
         });
       }
     } else {
-      res.send("please fill all details");
+      res.status(401).json({msg:"please fill all details"});
     }
   } catch (err) {
     console.log("err", err);
-    res.send(err);
+    res.status(401).json({msg:err});
   }
 });
 
@@ -48,21 +48,22 @@ userRoute.post("/login", async (req, res) => {
             process.env.privateKey,
             { expiresIn: "1h" }
           );
-          res.send({
+          res.status(201).json({
             msg: "Login Sucess",
             Token: token,
             email: user[0].email,
             name: user[0].name,
           });
         } else {
-          res.send("wrong password");
+          res.status(401).json({msg:"wrong password"});
         }
       });
     } else {
-      res.send("wrong email");
+      res.status(401).json({msg:"wrong email"});
     }
   } catch (err) {
     console.log("err", err);
+    res.status(401).json({msg:err});
   }
 });
 
